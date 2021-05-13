dbddbcb3630ef0f201461090e449c817dbbf8bed
Mutaskjn <mummy5799@gmail.com>
2021-05-13 12:51:29 +0300

    For loops By using matrix multiplication for loops are eliminated. The speed is increased a lot !!

--- main.py
+++ main.py
@@ -1,132 +1,99 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from numba import jit, cuda
+from matplotlib import pyplot as plt
+
 
 class Model:
+    def __init__(self):
+        self.loss = 0
+        self.stepSize = 1e-3
 
-    def __init__(self, inputs, labels):
-        self.labels = labels
+        self.m = 300
+        self.n = 784
+        self.d1 = 1000
+        self.d2 = 1000
+        self.K = 10
 
-        self.X = inputs
-        self.Y = []
+        self.W1 = np.random.randn(self.d1, self.n)*np.sqrt(2/self.n)
+        self.W2 = np.random.randn(self.d2, self.d1)*np.sqrt(2/self.d1)
+        self.W3 = np.random.randn(self.K, self.d2)*np.sqrt(2/self.d2)
 
-        self.loss = 1
-        self.stepSize = 1e-4
+        self.h2 = np.zeros(self.d1)
+        self.h1 = np.zeros(self.d2)
 
-        self.W1 = np.random.rand(1000,784)*1e-4
-        self.W2 = np.random.rand(500,1000)*1e-4
-        self.W3 = np.random.rand(10,500)*1e-4
+        self.gradW1 = np.zeros((self.d1, self.n), float)
+        self.gradW2 = np.zeros((self.d2, self.d1), float)
+        self.gradW3 = np.zeros((self.d2, self.d1), float)
 
-        self.B1 = np.random.rand(1000)*1e-4
-        self.B2 = np.random.rand(500)*1e-4
-        self.B3 = np.random.rand(10)*1e-4
+        self.prob = np.zeros((self.K, self.m))
 
-        self.h2 = []
-        self.h1 = []
+    def forward(self, X):
+        # get the # of inputs
+        self.m = X.shape[1]
 
-        self.gradW1 = np.zeros((1000,784), float)
-        self.gradW2 = np.zeros((500,1000), float)
-        self.gradW3 = np.zeros((10,500), float)
+        # first hidden layer
+        self.h1 = np.maximum(0, np.matmul(self.W1, X))
 
-        self.gradB1 = np.zeros(1000, float)
-        self.gradB2 = np.zeros(500, float)
-        self.gradB3 = np.zeros(10, float)
+        # second hidden layer
+        self.h2 = np.maximum(0, np.matmul(self.W2, self.h1))
 
-        self.prob = []
-        self.count = 1
+        # the result
+        prediction = np.matmul(self.W3, self.h2)
 
-    def forward(self):
-        #first hidden layer
-        self.h1 = [np.matmul(self.W1, np.transpose(self.X[i,:]))+self.B1 for i in range(self.X.shape[0])]
-        self.h1 = [np.maximum(0,self.h1[i]) for i in range(len(self.h1))]
-        
-        #second hidden layer
-        self.h2 = [np.matmul(self.W2, self.h1[i])+self.B2 for i in range(len(self.h1))]
-        self.h2 = [np.maximum(0,self.h2[i]) for i in range(len(self.h2))]
-        
-        #The result
-        self.Y = [np.matmul(self.W3, self.h2[i])+self.B3 for i in range(len(self.h2))]
+        return prediction
 
-    def backward(self):
-        #calculating gradient
-        #from loss function to y
-        logGrad = [np.multiply(-np.log(10), self.prob[i][self.labels[i]]) for i in range(len(self.prob))]
-        self.Y = [np.where(self.prob[i]==self.prob[i][self.labels[i]], self.prob[i][self.labels[i]], 0)\
-            -np.multiply(self.prob[i],self.prob[i][self.labels[i]]) for i in range(len(self.prob))]
-        self.Y = [np.multiply(1/logGrad[i], self.Y[i]) for i in range(len(self.Y))]
+    def backward(self, X, labels):
+        # back propagation
+        # from loss function to probabilities
+        prob_true = self.prob[labels, np.arange(self.prob.shape[1])]
+        grad_log = np.divide(1, np.multiply(-np.log(10), prob_true))
 
-        for i in range(len(self.Y)):
-            self.gradB3 = np.add(self.gradB3, self.Y[i])
+        # from probabilities to outputs
+        grad_Y = np.multiply(-prob_true, self.prob)
+        grad_Y[labels, np.arange(self.prob.shape[1])] += prob_true
+        grad_Y = np.multiply(grad_Y, grad_log)
 
-        #from y to W3 and h2
-        for i in range(len(self.Y)):
-            self.gradW3 = np.add(self.gradW3, np.matmul(np.reshape(self.Y[i], (10,1)), np.reshape((self.h2[i]), (1,500))))
+        # from outputs to W3
+        self.gradW3 = np.moveaxis(np.broadcast_to(grad_Y, (self.d2, self.K, self.m)), 0, 1)
+        self.gradW3 = np.mean(np.multiply(self.gradW3, np.broadcast_to(self.h2, (self.K, self.d2, self.m))), axis=-1)
 
-        gradB2 = np.zeros(500, float)
-        for i in range(len(self.Y)):
-            gradB2 = np.add(gradB2, np.matmul(np.transpose(self.W3), np.reshape((self.Y[i]), (10,1))).reshape(500))
+        # from outputs to h2
+        grad_h2 = np.matmul(np.transpose(self.W3), grad_Y)
+        grad_h2 = np.multiply(np.where(grad_h2 > 0, 1, 0), grad_h2)
 
-        #from h2 to W2 and h1
-        self.h2 = [np.multiply(gradB2, np.reshape(np.where(self.h2[i]>0, 1, 0), (1,500))) for i in range(len(self.h2))]
+        # from h2 to W2
+        self.gradW2 = np.moveaxis(np.broadcast_to(grad_h2, (self.d1, self.d2, self.m)), 0, 1)
+        self.gradW2 = np.mean(np.multiply(self.gradW2, np.broadcast_to(self.h1, (self.d2, self.d1, self.m))), axis=-1)
 
-        for i in range(len(self.Y)):
-            self.gradB2 = np.add(self.gradB2, self.h2[i])
+        # from h2 to h1
+        grad_h1 = np.matmul(np.transpose(self.W2), grad_h2)
+        grad_h1 = np.multiply(np.where(self.h1 > 0, 1, 0), grad_h1)
 
-        for i in range(len(self.Y)):
-            self.gradW2 = np.add(self.gradW2, np.matmul(np.reshape(self.h2[i], (500,1)), np.reshape((self.h1[i]), (1,1000))))
+        # from h1 to W1
+        self.gradW1 = np.moveaxis(np.broadcast_to(grad_h1, (self.n, self.d1, self.m)), 0, 1)
+        self.gradW1 = np.mean(np.multiply(self.gradW1, np.broadcast_to(X, (self.d1, self.n, self.m))), axis=-1)
 
-        gradB1 = np.zeros(1000, float)
-        for i in range(len(self.Y)):
-            gradB1 = np.add(gradB1, np.matmul(np.transpose(self.W2), np.reshape((self.h2[i]), (500,1))).reshape(1000))
+    def loss_function(self, Y, labels):
+        # calculating probabilities for all y
+        tmp = np.exp(Y)
+        self.prob = np.divide(tmp, np.sum(tmp, axis=0))
 
-        #from h1 to W1
-        self.h1 = [np.multiply(gradB1, np.reshape(np.where(self.h1[i]>0, 1, 0), (1,1000))) for i in range(len(self.h1))]
+        # calculating loss function
+        self.loss = -np.mean(np.log10(self.prob[labels, np.arange(self.prob.shape[1])]))
 
-        for i in range(len(self.Y)):
-            self.gradB1 = np.add(self.gradB1, self.h1[i])
-
-        for i in range(len(self.Y)):
-            self.gradW1 = np.add(self.gradW1, np.matmul(np.reshape(self.h1[i], (1000,1)), np.reshape(self.X[i,:], (1,784))))
-
-    def loss_function(self):
-        #calculating probabilities for all y
-        self.prob = [np.exp(self.Y[i])/np.sum(np.exp(self.Y[i])) for i in range(len(self.Y))]
-        entropy = [-np.log10(self.prob[i][self.labels[i]]) for i in range(len(self.prob))] #just take the probability of the true classes
-        self.loss = sum(entropy)/len(entropy)
-        print(self.count, "   ", self.loss)
-        self.count+=1
-        
     def step(self):
-        self.gradW1 = self.gradW1/len(self.Y)
-        self.gradW2 = self.gradW2/len(self.Y)
-        self.gradW3 = self.gradW3/len(self.Y)
-
         self.W1 -= self.stepSize*self.gradW1
         self.W2 -= self.stepSize*self.gradW2
         self.W3 -= self.stepSize*self.gradW3
 
-        self.B1 = self.B1/len(self.Y)
-        self.B2 = self.B2/len(self.Y)
-        self.B3 = self.B3/len(self.Y)
+    def clear_variables(self):
 
-        self.B1 -= self.stepSize*self.gradB1[0]
-        self.B2 -= self.stepSize*self.gradB2[0]
-        self.B3 -= self.stepSize*self.gradB3[0]
-        
-    def clearVariables(self):
-        self.Y.clear()
-        self.h1.clear()
-        self.h2.clear()
-        self.prob.clear()
+        self.prob = np.zeros((self.K, self.m))
 
-        self.gradW1 = np.zeros((1000,784), float)
-        self.gradW2 = np.zeros((500,1000), float)
-        self.gradW3 = np.zeros((10,500), float)
-
-        self.gradB1 = np.zeros(1000, float)
-        self.gradB2 = np.zeros(500, float)
-        self.gradB3 = np.zeros(10, float)
+        self.gradW1 = np.zeros((self.d1, self.n), float)
+        self.gradW2 = np.zeros((self.d2, self.d1), float)
+        self.gradW3 = np.zeros((self.d2, self.d1), float)
 
     def test(self, testX):
         #first hidden layer
@@ -142,36 +109,62 @@ class Model:
 
         return Y
 
+
+def accuracy(calculatedY, label):
+
+    prediction = calculatedY.argmax(axis=0)
+
+    acc = np.sum((prediction == label))/len(label)
+
+    return acc
+
+
 if __name__ == '__main__':
     
     pathToTrainingData = "digit-recognizer/train.csv"
 
-    #getting the training data 
+    # getting the training data
     df = pd.read_csv(pathToTrainingData)
 
-    label = df["label"].to_numpy() #get labels
+    label = df["label"].to_numpy()  # get labels
 
-    df.drop(["label"], axis="columns", inplace=True) #drop the labels, get the image data
+    df.drop(["label"], axis="columns", inplace=True)  # drop the labels, get the image data
     dataset = df.to_numpy()
 
-    #splitting the data into training and validation sets
+    # splitting the data into training and validation sets
     trainSet, validationSet, labelTrainSet, labelValSet = train_test_split(dataset, label, train_size=0.8, random_state=42, stratify=label)
 
-    #Training
-    model = Model(trainSet[labelTrainSet == 2], labelTrainSet[labelTrainSet == 2])
+    # Training
+    trainSet = np.true_divide(trainSet, 255)  # normalization
+    X = np.transpose(trainSet[0:300])
+    Y = labelTrainSet[0:300]
 
-    while(model.loss > 0.1):
+    # initializations for model
+    model = Model()
+    numIter = 1000
+    accuracyTrain = np.zeros(numIter)
+    accuracyTest = np.zeros(numIter)
 
-        model.forward() #forward calculation
+    # iterations
+    for i in range(numIter):
 
-        model.loss_function() #loss calculation
+        Y_predict = model.forward(X)  # forward calculation
 
-        model.backward() #back propogation
+        model.loss_function(Y_predict, Y)  # loss calculation
 
-        model.step() #take the step to the optimum
+        model.backward(X, Y)  # back propagation
 
-        model.clearVariables()
+        model.step()  # take the step to the optimum
 
-    print(model.test(trainSet[0:10]))
-    print(labelTrainSet[0:10])
+        model.clear_variables()
 
+        accuracyTrain[i] = accuracy(Y_predict, Y)  # Accuracy calculation on the training and validation
+
+        print(i, "  ", accuracyTrain[i], "  ", model.loss)
+
+    plt.plot(np.arange(numIter), accuracyTrain)
+    plt.show()
+
+    pd.DataFrame(model.W1).to_csv("W1.csv")
+    pd.DataFrame(model.W2).to_csv("W2.csv")
+    pd.DataFrame(model.W3).to_csv("W3.csv")
